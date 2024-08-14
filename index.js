93fe772531be39aed127682adaf4cb4d35529e3a
akybreaky <153144098+akybreaky@users.noreply.github.com>
2024-08-14 18:45:15 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 // A) Function: findSummation
 function findSummation(number = 1) {
